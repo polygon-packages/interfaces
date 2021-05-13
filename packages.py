b6b1f05fdf80e1f78c8363f2dfeec1bcf6d81097
justaprudev <justaprudev@gmail.com>
2021-05-13 11:06:55 +0530

    packages: dont touch the db

--- packages.py
+++ packages.py
@@ -7,9 +7,6 @@ async def pkg_interface_add(event):
     package_supported = polygon.add_package(url)
 
     if package_supported is True:
-        packages = db.get("packages")
-        packages.append(url)
-        db.add("packages", list(set(packages)))
         await event.edit("`Installed package successfully.`")
     else:
         await event.edit(f"The following error occured while installing the package:\n{package_supported}")
@@ -19,9 +16,6 @@ async def pkg_interface_remove(event):
     # Very basic telegram interface to remove packages
     name = event.pattern_match.group(1)
     if polygon.remove_package(name):
-        packages = db.get("packages")
-        packages.remove(name)
-        db.add("packages", packages)
         await event.edit(f"Removed package `{name}` successfully.")
     else:
         await event.edit(f"Package `{name}` not found.")
